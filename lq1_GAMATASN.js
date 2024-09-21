d20d0a57f2d36756cc74546d15cb0ced0c81e6b4
Gamata Sophia Nicole M. <mecossophia@gmail.com>
2024-09-21 12:11:56 -0400

    initial commit

---
+++ lq1_GAMATASN.js
@@ -0,0 +1,36 @@
+//person 1
+let P1name=  "Maria Delos Santos Las Marias"
+let P1age = 25;
+let P1address = "Upper Session Road,Baguio City,Benguet";
+//person 2
+let P2name = "Juan Gamoso Dela Cruz"
+let P2age = 28;
+let P2address = "San Nicolas, Candon City,Ilocos Sur";
+
+
+//OPERATIONS
+let sum = P1age + P2age;
+console.log(sum);
+
+let result = (P1age + P2age) - P1name.length;
+result = result * P2name.length;
+console.log(result);
+
+let res = Math.pow(P1address.length, P2address.length);
+console.log(res);
+
+//console 1
+console.log(P1name);
+console.log(P1age);
+console.log(P1address);
+console.log(P2name);
+console.log(P2age);
+console.log(P2address);
+
+//length
+console.log(P1name.length);
+console.log(P1age);
+console.log(P1address.length);
+console.log(P2name.length);
+console.log(P2age);
+console.log(P2address.length);
